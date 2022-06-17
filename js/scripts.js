7461cedb4fd7a11b99ee38d71656b7fca181434b
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-17 09:24:05 -0700

    add logic to make the first rule run true

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,6 @@
-
-// Business Logic for Contacts ---------
 function Pizza(firstName, lastName) {
   this.firstName = firstName;
   this.lastName = lastName;
 }
-
-Pizza.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-};
-
-// User Interface Logic ---------
-let customerBook = new CustomerBook();
-
-$(document).ready(function() {
-  $("form#newOne").submit(function(event) {
-    event.preventDefault();
-    const inputtedSize = $("#size").val();
-    const inputtedTopping = $("#top").val();
-    let newPizza = new Pizza(inputtedSize, inputtedTopping);
-    // customerBook.addContact(newContact);
-    // console.log(customerBook.contacts);
-  });
-});
+let newzaa = new Pizza("pepperoni", "medium")
+console.log(newzaa);
