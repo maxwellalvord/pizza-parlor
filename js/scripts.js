b41145555ff79e6125799a71bb94fe82671b73c4
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-17 09:36:07 -0700

    add logic to console.log the users inputted pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
-function Pizza(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+function Pizza(topping, size) {
+  this.topping = topping;
+  this.size = size;
 }
-let newzaa = new Pizza("pepperoni", "medium")
-console.log(newzaa);
+// let newzaa = new Pizza("pepperoni", "medium")
+// console.log(newzaa);
+
+
+
+
+
+
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+    const psize = $("#size").val();
+    const top = $("#top").val();
+    let newzaa = new Pizza(psize, top);
+    console.log(newzaa);
+  });
+});
