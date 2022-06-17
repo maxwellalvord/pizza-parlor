ebd29328f800694951daa6a810c67713b53ef9e0
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-17 09:11:25 -0700

    WIP: add address book framework to solve for first test

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,24 @@
-// Business Logic
-function Pizza(){
-  this.topping = "";
-  this.size = "";
-  this.price = 0;
+
+// Business Logic for Contacts ---------
+function Pizza(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
 }
 
+Pizza.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
 
+// User Interface Logic ---------
+let customerBook = new CustomerBook();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//UI Logic
 $(document).ready(function() {
-  $("#subform").submit(function(event) {
-    let newPizza = new Pizza()
+  $("form#newOne").submit(function(event) {
+    event.preventDefault();
+    const inputtedSize = $("#size").val();
+    const inputtedTopping = $("#top").val();
+    let newPizza = new Pizza(inputtedSize, inputtedTopping);
+    // customerBook.addContact(newContact);
+    // console.log(customerBook.contacts);
   });
 });
